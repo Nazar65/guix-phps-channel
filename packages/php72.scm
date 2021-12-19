19e0dd32e692c27439a2d1ee3c6fbe8658504384
Nazar Klovanych <nazarn96@gmail.com>
2021-12-20 01:18:28 +0200

    Add dependency

--- packages/php72.scm
+++ packages/php72.scm
@@ -30,6 +30,7 @@
   #:use-module (gnu packages compression)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages cyrus-sasl)
+  #:use-module (gnu packages crypto)
   #:use-module (gnu packages databases)
   #:use-module (gnu packages dbm)
   #:use-module (gnu packages fontutils)

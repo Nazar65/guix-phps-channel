3616dd4a3254073345660d0567190b02031be939
Nazar Klovanych <nazarn96@gmail.com>
2021-12-19 23:56:38 +0200

    Fix sha2 hash

--- packages/php72.scm
+++ packages/php72.scm
@@ -68,7 +68,7 @@
                                   "php-" version ".tar.xz"))
               (sha256
                (base32
-                "00wr6zc24y7f0ls0yiw1fkhq3zfx43463g24ziyp061cday137j0"))
+                "0nwx0kah9svx1qgcb042lh2hgn5c2s4rrp88mccw3ni0nri9yl8r"))
               (modules '((guix build utils)))
               (snippet
                '(with-directory-excursion "ext"

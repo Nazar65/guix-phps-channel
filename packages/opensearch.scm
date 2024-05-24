8bc601ec150bf6b87913ee419584a5bad21bc913
Nazar Klovanych <nazarn96@gmail.com>
2024-05-24 10:46:13 +0300

    Fix builds, update version

--- packages/opensearch.scm
+++ packages/opensearch.scm
@@ -28,29 +28,60 @@
   #:use-module (gnu packages)
   #:use-module (guix download)
   #:use-module (guix licenses)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages java)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages pkg-config)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public opensearch
   (package
-    (name "openserarch")
-    (version "2.5.0")
+    (name "opensearch")
+    (version "2.14.0")
     (source (origin
               (method url-fetch)
 	      (uri
-	       (string-append "https://artifacts.opensearch.org/releases/bundle/opensearch/" version "/opensearch-" version" -linux-x64.tar.gz"))
+	       (string-append "https://artifacts.opensearch.org/releases/bundle/opensearch/" version "/opensearch-" version "-linux-x64.tar.gz"))
 	      (sha256
                (base32
-		"055khpw0j8zmax3j9yma6qz9cdw5dnri218wiwhlnz1wqw0h4q3j"))))
+		"0xpcqlz4y32flzhdk9a0cqlffs77rljhg32q1ssxlxl3f7dyswil"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
-       (modify-phases %standard-phases)
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (delete 'build)
+         (replace 'install
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (bin (string-append out "/bin"))
+                    (config (string-append out "/config"))
+                    (lib (string-append out "/lib"))
+                    (modules (string-append out "/modules"))
+                    (plugins (string-append out "/plugins"))
+                    (jdk-opensearch (string-append out "/jdk"))
+                    (coreutils (string-append (assoc-ref inputs "coreutils")
+                                    "/bin")))
+               (copy-recursively "./bin" bin)
+               (copy-recursively "./config" config)
+               (copy-recursively "./lib" lib)
+               (copy-recursively "./modules" modules)
+               (copy-recursively "./plugins" plugins)
+               (wrap-program (string-append out "/bin/opensearch")
+                 `("JAVA_HOME" ":" = (,(assoc-ref inputs "jdk")))
+                 `("PATH" ":" prefix ,(list coreutils))
+                 `("LD_LIBRARY_PATH" ":" prefix
+                                      (,(string-append out "plugins/opensearch-knn/lib"))))
+               (wrap-program (string-append out "/bin/opensearch-plugin")
+                 `("JAVA_HOME" ":" = (,(assoc-ref inputs "jdk"))))
+               ))))
        #:parallel-build? #f
        #:tests? #f))
+    (inputs
+     `(("jdk" ,openjdk11)
+       ("coreutils" ,coreutils)))
     (home-page "https://github.com/opensearch-project")
-    (synopsis "OpenSearch is a community-driven, Apache 2.0-licensed open source search and analytics suite")
+    (synopsis "Open Source, Distributed, RESTful Search Engine")
     (description "OpenSearch is a community-driven, Apache 2.0-licensed open source search and analytics suite that makes it easy to ingest, search, visualize, and analyze data. Developers build with OpenSearch for use cases such as application search, log analytics, data observability, data ingestion, and more.")
     (license (non-copyleft "file://COPYING"
                            "See COPYING file in the distribution."))))
